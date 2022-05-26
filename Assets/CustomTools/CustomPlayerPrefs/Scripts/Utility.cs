c7c775da523b22fce94c1fd27f46d8b5879c0a92
Ertan Turan <ertanturan@outlook.com>
2022-05-26 17:48:12 +0300

    Utilities moved to one proper class

--- Assets/CustomTools/CustomPlayerPrefs/Scripts/Utility.cs
+++ Assets/CustomTools/CustomPlayerPrefs/Scripts/Utility.cs
@@ -2,6 +2,9 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using UnityEngine;
 
     public static class Utility
     {
@@ -19,6 +22,30 @@
             }
 
             return -1;
+        }
+        
+        public static async Task SaveToJsonAsync<T>(T objectToSerialize, string fullPath)
+        {
+            string jsonString = JsonUtility.ToJson(objectToSerialize, true);
+
+            using (StreamWriter streamWriter = new StreamWriter(fullPath))
+            {
+                await streamWriter.WriteAsync(jsonString);
+            }
+        }
+
+        public static async Task<T> ReadFromJsonAsync<T>(string fullPath)
+        {
+            T returnedValue;
+
+            using (StreamReader streamReader = new StreamReader(fullPath))
+            {
+                string jsonString = await streamReader.ReadToEndAsync();
+                returnedValue = JsonUtility.FromJson<T>(jsonString);
+            }
+
+
+            return returnedValue;
         }
     }
 }
